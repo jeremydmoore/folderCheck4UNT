f6c43f8033c6bd473a95fee31684cf0479bfa3ad
photosbyjeremy <alt.photosbyjeremy@gmail.com>
2013-10-27 20:48:48 -0500

    Update folderCheck.py
    moved result = build_dict() into main(), added a docstring to main(), and added a check for folders without tiffs that writes those directories to a text file.
    
    Also added instructions on how to see the errors on the console, e.g. print "cat naming_errors.txt to see folders with problems"

--- folderCheck.py
+++ folderCheck.py
@@ -32,14 +32,23 @@ def naming_errors():
     """ appends folders with naming errors to the already created text """
     name_error = open("naming_errors_images.txt", "a")
     print >>name_error, folders
+    name_error.close()
     naming_errors += 1
 
-def test_identifiers():
+def main():
+    """ Check identifiers against the folder name
+    
+    Magicknumbers will be tested for first, by checking images[0] against the usual
+    use cases. Then checking for over 100 images in a folder against 3 digit filenames,
+    2-99 images against 2 digit filenames, negatives/slides, and finally if the folder
+    is empty it is added to a text file. Still need to add serialized images to the list
+    """
     total_tiffs = 0
     total_negatives_slides = 0
     total_docs_prints= 0
     total_magicknumbered = 0
     naming_errors = 0
+    result = build_dict()
     for folders, images in result.iteritems():
 	if images[0] == "00010001.tif":
 	    total_magicknumbered += 1
@@ -63,7 +72,7 @@ def test_identifiers():
 		    naming_errors_zero()
 		else:
 		    naming_errors()
-	if len(images) == 1:
+	elif len(images) == 1:
 	    if (folders + ".tif") == images[0]:
 		total_negatives_slides += 1
 	    else:
@@ -71,9 +80,16 @@ def test_identifiers():
 		    naming_errors_zero()
 		else:
 		    naming_errors()
+	elif len(images) == 0:
+		no_tiffs = open("no_tiffs.txt", "a")
+                print >>no_tiffs, folders
+                no_tiffs.close()
+                print "There are directories without tiffs"
+                print "cat no_tiffs.txt to see these folders"
 	total_tiffs += len(images)
     if naming_errors >= 1:
     	print ("There are %s mismatched identifiers." % (naming_errors))
+    	print "cat naming_errors to see folders with problems"
     if total_negatives_slides >= 1:
 	print ("There are %s TIFFs that appear to be negatives and/or slides." % (total_negatives_slides))
     if total_docs_prints >= 1:
@@ -82,8 +98,5 @@ def test_identifiers():
 	print ("There are %s magicknumbered items" % (total_magicknumbered))
     print ("There are %s TIFFs" % (total_tiffs))
 
-
-
 if __name__ == "__main__":
-    result = build_dict()
-    test_identifiers()
+    main()
